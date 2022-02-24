253e0e9bed12d6af344486c8dbf57f0ad1835316
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-02-24 14:13:11 -0600

    pre-commit clean-up on all files

--- pytorch_gleam/modeling/models/unified_qa.py
+++ pytorch_gleam/modeling/models/unified_qa.py
@@ -1,10 +1,9 @@
-from typing import Dict, Optional
+from typing import Optional
 
 import torch
 from transformers import Adafactor
 from transformers.optimization import get_adafactor_schedule, get_constant_schedule
 
-from pytorch_gleam.modeling.metrics import Metric
 from pytorch_gleam.modeling.models.base_models import BaseLanguageModelForSeq2SeqLM
 from pytorch_gleam.qa import MultiQATaskModule
 
@@ -14,13 +13,11 @@ class UnifiedQAForConditionalGeneration(BaseLanguageModelForSeq2SeqLM):
     def __init__(
         self,
         qa_task: MultiQATaskModule,
-        task_metrics: Dict[str, Metric],
         *args,
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
         self.qa_task = qa_task
-        self.task_metrics = task_metrics
 
     def eval_epoch_end(self, outputs, stage):
         loss = torch.stack([x["loss"] for x in outputs], dim=0).mean().cpu()
