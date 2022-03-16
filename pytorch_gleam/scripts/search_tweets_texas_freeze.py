36f240d54baaa4dcb0515fc16b5afadd20c67f07
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-03-16 14:58:46 -0500

    Working on Texas freeze research

--- pytorch_gleam/scripts/search_tweets_texas_freeze.py
+++ pytorch_gleam/scripts/search_tweets_texas_freeze.py
@@ -206,8 +206,6 @@ if __name__ == "__main__":
         search_terms.insert(0, default_search_query)
 
     query = "query=" + " ".join(search_terms)
-    print(query)
-    exit()
     max_results = "max_results=500"
     tweet_fields = (
         "tweet.fields="
