b5edc0249f8ea5d1e6830ae14bc37890c5fc492c
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-03-24 11:09:37 -0500

    Working on Texas freeze research

--- pytorch_gleam/scripts/process_raw_tweets.py
+++ pytorch_gleam/scripts/process_raw_tweets.py
@@ -31,9 +31,7 @@ def read_jsonl(path):
 def invert_errors(errors):
     inv = defaultdict(dict)
     for error in errors:
-        if "resource_id" not in error:
-            print(error)
-        r_id = error["resource_id"]
+        r_id = error["resource_id"] if "resource_id" in error else error["value"]
         e_type = error["detail"].replace(f": [{r_id}].", "")
         inv[r_id][e_type] = error
     return inv
