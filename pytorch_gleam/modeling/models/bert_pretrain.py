9d322f809b46bca3e604f5ee17c41c52afaae5ae
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-04-05 19:52:44 -0500

    Working on vaccine bert

--- pytorch_gleam/modeling/models/bert_pretrain.py
+++ pytorch_gleam/modeling/models/bert_pretrain.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import AdamW
+import torch.nn as nn
 
 from pytorch_gleam.modeling.models.base_models import BaseLanguageModelForPreTraining
 
@@ -12,6 +12,7 @@ class BertPreTrainLanguageModel(BaseLanguageModelForPreTraining):
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
+        self.loss_func = nn.CrossEntropyLoss(reduction="none")
 
     def eval_epoch_end(self, outputs, stage):
         loss = torch.stack([x["loss"] for x in outputs], dim=0).mean().cpu()
@@ -26,14 +27,22 @@ class BertPreTrainLanguageModel(BaseLanguageModelForPreTraining):
             input_ids=batch["input_ids"],
             attention_mask=batch["attention_mask"],
             token_type_ids=batch["token_type_ids"],
-            labels=batch["masked_lm_labels"],
-            next_sentence_label=batch["next_sentence_labels"],
+            # labels=batch["masked_lm_labels"],
+            # next_sentence_label=batch["next_sentence_labels"],
         )
+        labels = batch["masked_lm_labels"]
+        next_sentence_label = batch["next_sentence_labels"]
+
         # TODO more metrics than just loss
-        # prediction_logits = outputs.prediction_logits
-        # seq_relationship_logits = outputs.seq_relationship_logits
-        loss = outputs.loss
-        return loss
+        prediction_logits = outputs.prediction_logits
+        seq_relationship_logits = outputs.seq_relationship_logits
+        masked_lm_loss = self.loss_func(prediction_logits, labels)
+        next_sentence_loss = self.loss_func(seq_relationship_logits, next_sentence_label)
+        masked_lm_loss = masked_lm_loss.sum()
+        next_sentence_loss = next_sentence_loss.sum()
+        total_loss = masked_lm_loss + next_sentence_loss
+
+        return total_loss
 
     def training_step(self, batch, batch_idx):
         loss = self(batch)
@@ -50,13 +59,13 @@ class BertPreTrainLanguageModel(BaseLanguageModelForPreTraining):
 
     def configure_optimizers(self):
         params = self._get_optimizer_params(self.weight_decay)
-        # optimizer = torch.optim.AdamW(params, lr=self.learning_rate, weight_decay=self.weight_decay)
-        optimizer = AdamW(
-            params,
-            lr=self.learning_rate,
-            weight_decay=self.weight_decay,
-            correct_bias=False,
-        )
+        optimizer = torch.optim.AdamW(params, lr=self.learning_rate, weight_decay=self.weight_decay)
+        # optimizer = AdamW(
+        #     params,
+        #     lr=self.learning_rate,
+        #     weight_decay=self.weight_decay,
+        #     correct_bias=False,
+        # )
         # scheduler = get_constant_schedule(optimizer)
         # opt_dict = {
         #     "optimizer": optimizer,
