0ec79c929f319da12555bd878094dbf3eb7679ae
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-04-06 18:08:00 -0500

    Working on vaccine bert

--- pytorch_gleam/modeling/models/bert_pretrain.py
+++ pytorch_gleam/modeling/models/bert_pretrain.py
@@ -1,9 +1,6 @@
 import torch
-import torch.nn as nn
 
 from pytorch_gleam.modeling.models.base_models import BaseLanguageModelForPreTraining
-
-# from transformers import AdamW
 
 
 # noinspection PyAbstractClass
@@ -14,8 +11,6 @@ class BertPreTrainLanguageModel(BaseLanguageModelForPreTraining):
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
-        # self.loss_func = nn.CrossEntropyLoss(reduction="none")
-        self.loss_func = nn.CrossEntropyLoss()
 
     def eval_epoch_end(self, outputs, stage):
         loss = torch.stack([x["loss"] for x in outputs], dim=0).mean().cpu()
@@ -33,37 +28,7 @@ class BertPreTrainLanguageModel(BaseLanguageModelForPreTraining):
             labels=batch["masked_lm_labels"],
             next_sentence_label=batch["next_sentence_labels"],
         )
-        # outputs = self.lm(
-        #     input_ids=batch["input_ids"],
-        #     attention_mask=batch["attention_mask"],
-        #     token_type_ids=batch["token_type_ids"],
-        #     # labels=batch["masked_lm_labels"],
-        #     # next_sentence_label=batch["next_sentence_labels"],
-        # )
-        # labels = batch["masked_lm_labels"].view(-1)
-        # labels = batch["masked_lm_labels"]
-        # next_sentence_label = batch["next_sentence_labels"]
-        # labels_mask = (~labels.eq(-100)).float()
-        # labels_count = labels_mask.sum()
-        # labels = labels * labels_mask.long()
-
-        # TODO more metrics than just loss
-        # prediction_logits = outputs.prediction_logits
-        # seq_relationship_logits = outputs.seq_relationship_logits
-
-        # masked_lm_loss = self.loss_func(prediction_logits.view(-1, self.lm.config.vocab_size), labels.view(-1))
-        # masked_lm_loss = self.loss_func(prediction_logits.view(-1, self.lm.config.vocab_size), labels)
-
-        # next_sentence_loss = self.loss_func(seq_relationship_logits.view(-1, 2), next_sentence_label.view(-1))
-        # masked_lm_loss = (masked_lm_loss * labels_mask).sum() / labels_count
-        # next_sentence_loss = next_sentence_loss.mean()
-        # total_loss = masked_lm_loss + next_sentence_loss
-        # total_loss = next_sentence_loss
-        # this is incorrect, just doing it to test TPUs
-        # total_loss = seq_relationship_logits.mean()
         total_loss = outputs.loss
-        # TODO only for now
-        # total_loss = next_sentence_loss
         return total_loss
 
     def training_step(self, batch, batch_idx):
@@ -80,31 +45,5 @@ class BertPreTrainLanguageModel(BaseLanguageModelForPreTraining):
         return results
 
     def configure_optimizers(self):
-        # params = self._get_optimizer_params(self.weight_decay)
         optimizer = torch.optim.AdamW(self.parameters(), lr=self.learning_rate, weight_decay=0.0)
-        # TODO fix
-        # optimizer = torch.optim.SGD(self.parameters(), lr=self.learning_rate)
-        # optimizer = AdamW(
-        #     params,
-        #     lr=self.learning_rate,
-        #     weight_decay=self.weight_decay,
-        #     correct_bias=False,
-        # )
-        # scheduler = get_constant_schedule(optimizer)
-        # opt_dict = {
-        #     "optimizer": optimizer,
-        #     "lr_scheduler": {
-        #         # REQUIRED: The scheduler instance
-        #         "scheduler": scheduler,
-        #         # The unit of the scheduler's step size, could also be 'step'.
-        #         # 'epoch' updates the scheduler on epoch end whereas 'step'
-        #         # updates it after a optimizer update.
-        #         "interval": "step",
-        #         # How many epochs/steps should pass between calls to
-        #         # `scheduler.step()`. 1 corresponds to updating the learning
-        #         # rate after every epoch/step.
-        #         "frequency": 1,
-        #     },
-        # }
-        # return opt_dict
         return optimizer
