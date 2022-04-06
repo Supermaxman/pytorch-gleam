a0a4cfa0caa030901d70e753576259b523117027
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-04-06 16:46:17 -0500

    Working on vaccine bert

--- pytorch_gleam/callbacks/progress.py
+++ pytorch_gleam/callbacks/progress.py
@@ -10,12 +10,12 @@ class TPURichProgressBar(RichProgressBar):
     def _init_progress(self, trainer):
         if self.is_enabled and (self.progress is None or self._progress_stopped):
             self._reset_progress_bar_ids()
-            self._console: Console = Console()
+            self._console = Console(**self._console_kwargs)
             self._console.clear_live()
             self.progress = CustomProgress(
                 *self.configure_columns(trainer),
-                refresh_per_second=self._refresh_rate_per_second if self._refresh_rate_per_second > 0 else 1,
-                disable=self._enabled and self._refresh_rate_per_second > 0,
+                auto_refresh=False,
+                disable=self.is_disabled,
                 console=self._console,
             )
             self.progress.start()
