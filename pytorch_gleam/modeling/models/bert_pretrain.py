88bf1c09865f2119fab71b332ec0ac8808d46f55
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-04-11 09:09:46 -0500

    Working on vaccine bert

--- pytorch_gleam/modeling/models/bert_pretrain.py
+++ pytorch_gleam/modeling/models/bert_pretrain.py
@@ -1,4 +1,5 @@
 from torch.optim import AdamW
+from torch.optim.lr_scheduler import LambdaLR
 from transformers import Adafactor
 
 from pytorch_gleam.modeling.models.base_models import BaseLanguageModelForPreTraining
@@ -9,10 +10,15 @@ class BertPreTrainLanguageModel(BaseLanguageModelForPreTraining):
     def __init__(
         self,
         optimizer: str = "adamw",
+        warmup_steps: int = 10000,
+        train_steps: int = 10000000,
         *args,
         **kwargs,
     ):
         self.optimizer = optimizer.lower()
+        # self.weight_decay = 0.01
+        self.warmup_steps = warmup_steps
+        self.train_steps = train_steps
         super().__init__(*args, **kwargs)
 
     def eval_epoch_end(self, outputs, stage):
@@ -59,6 +65,46 @@ class BertPreTrainLanguageModel(BaseLanguageModelForPreTraining):
                 warmup_init=False,
                 lr=self.learning_rate,
             )
+        elif self.optimizer == "bert":
+            params = self._get_optimizer_params(self.weight_decay)
+            opt = AdamW(params, lr=self.learning_rate, weight_decay=self.weight_decay, betas=(0.9, 0.999), eps=1e-6)
+
+            scheduler = BertLR(
+                opt,
+                num_warmup_steps=self.warmup_steps,
+                num_training_steps=self.train_steps,
+            )
+            optimizer = {
+                "optimizer": opt,
+                "lr_scheduler": {
+                    # REQUIRED: The scheduler instance
+                    "scheduler": scheduler,
+                    # The unit of the scheduler's step size, could also be 'step'.
+                    # 'epoch' updates the scheduler on epoch end whereas 'step'
+                    # updates it after a optimizer update.
+                    "interval": "step",
+                    # How many epochs/steps should pass between calls to
+                    # `scheduler.step()`. 1 corresponds to updating the learning
+                    # rate after every epoch/step.
+                    "frequency": 1,
+                },
+            }
         else:
             raise ValueError(f"Unknown optimizer: {self.optimizer}")
         return optimizer
+
+
+class BertLR(LambdaLR):
+    def __init__(self, optimizer, num_warmup_steps: int, num_training_steps: int, last_epoch=-1):
+        self.num_warmup_steps = num_warmup_steps
+        self.num_training_steps = num_training_steps
+        super().__init__(optimizer=optimizer, lr_lambda=self.lr_lambda, last_epoch=last_epoch)
+
+    def lr_lambda(self, current_step: int):
+        if current_step < self.num_warmup_steps:
+            return float(current_step) / float(max(1, self.num_warmup_steps))
+        return max(
+            0.0,
+            float(self.num_training_steps - current_step)
+            / float(max(1, self.num_training_steps - self.num_warmup_steps)),
+        )
