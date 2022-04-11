cdf4d3371ac84046ac9b171cdc99fe8a6c3994fb
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-04-11 10:16:21 -0500

    Working on vaccine bert

---
+++ pytorch_gleam/scripts/search_tweets_flu.py
@@ -0,0 +1,143 @@
+import itertools
+import json
+import os
+import time
+from datetime import date, timedelta
+
+import requests
+from tqdm import tqdm
+
+
+def format_endpoint(endpoint_url, fields):
+    endpoint = endpoint_url + "?" + "&".join(fields)
+    return endpoint
+
+
+def date_range(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n), start_date + timedelta(n + 1)
+
+
+def main():
+    start_date = date(2008, 1, 1)
+    end_date = date(2022, 1, 1)
+    # wait 5 seconds between queries
+    q_delay = 5
+
+    output_path = "/users/max/data/corpora/flu-vaccine-twitter/raw-v1"
+    secrets_path = "private/secrets.json"
+    with open(secrets_path, "r") as f:
+        secrets = json.load(f)["twitter"]
+    endpoint_url = "https://api.twitter.com/2/tweets/search/all"
+
+    # terms which directly refer to vaccine
+    # https://www.cdc.gov/flu/prevent/different-flu-vaccines.htm
+    vaccine_terms = [
+        "Afluria",
+        "Fluarix",
+        "Fluzone",
+        "Flublok",
+        "Flulaval",
+        "Flucelvax",
+        "Fluad",
+        "FluMist",
+        "Quadrivalent",
+    ]
+    vaccine_query = " OR ".join(vaccine_terms)
+    # terms which refer to disease vaccine is for
+    disease_terms = ["influenza", "flu", "H1N1", "H2N2", "H3N2", "H7N9", "H5N1", "influenzas", "influenze", "flus"]
+    disease_query = " OR ".join(disease_terms)
+
+    # terms which identify vaccination towards disease
+    vaccinate_terms = [
+        "jab",
+        "vax",
+        "vaccine",
+        "vaccines",
+        "vaccinate",
+        "vaccinated",
+        "vaccination",
+    ]
+    vaccinate_query = " OR ".join(vaccinate_terms)
+
+    joint_terms = [f"({d} {v})" for d, v in itertools.product(disease_terms, vaccinate_terms)]
+    joint_terms += vaccine_terms
+    # vaccine OR (disease and vaccinate)
+    search_query = f"{vaccine_query} OR (({disease_query}) ({vaccinate_query}))"
+
+    query = f"query={search_query} lang:en -is:retweet"
+    max_results = "max_results=500"
+    tweet_fields = (
+        "tweet.fields="
+        "id,text,author_id,created_at,conversation_id,public_metrics,"
+        "in_reply_to_user_id,referenced_tweets,lang,attachments,geo,"
+        "possibly_sensitive,reply_settings,source,withheld,entities"
+    )
+    user_fields = (
+        "user.fields=id,created_at,description,name,public_metrics,url," "username,verified,location,withheld,entities"
+    )
+    expansions = (
+        "expansions=author_id,in_reply_to_user_id,referenced_tweets.id,"
+        "referenced_tweets.id.author_id,attachments.media_keys,geo.place_id,"
+        "entities.mentions.username"
+    )
+    media_fields = "media.fields=duration_ms,height,media_key,preview_image_url,type," "url,width,public_metrics"
+    place_fields = "place.fields=contained_within,country,country_code,full_name,geo,id," "name,place_type"
+
+    all_dates = list(date_range(start_date, end_date))
+    for q_date_start, q_date_end in tqdm(all_dates, total=len(all_dates)):
+        start_time = q_date_start.strftime("%Y-%m-%dT%H:%M:%SZ")
+        end_time = q_date_end.strftime("%Y-%m-%dT%H:%M:%SZ")
+        next_token = ""
+        page_idx = 0
+        completed_file = q_date_start.strftime("%Y%m%dT%H%M%SZ") + q_date_end.strftime("%Y%m%dT%H%M%SZ") + ".lock"
+        completed_path = os.path.join(output_path, completed_file)
+        if os.path.exists(completed_path):
+            continue
+        num_results = 0
+        while next_token is not None:
+            result_name = (
+                q_date_start.strftime("%Y%m%dT%H%M%SZ") + q_date_end.strftime("%Y%m%dT%H%M%SZ") + f"-{page_idx}.json"
+            )
+            result_path = os.path.join(output_path, result_name)
+            fields = [
+                query,
+                f"start_time={start_time}",
+                f"end_time={end_time}",
+                max_results,
+                tweet_fields,
+                user_fields,
+                expansions,
+                media_fields,
+                place_fields,
+            ]
+            if len(next_token) > 0:
+                fields.append(f"next_token={next_token}")
+            endpoint = format_endpoint(endpoint_url, fields)
+            headers = {"Authorization": f"Bearer {secrets['bearer_token']}"}
+            try:
+                results = requests.get(endpoint, headers=headers).json()
+                with open(result_path, "w") as f:
+                    json.dump(results, f)
+            except Exception as e:
+                print(e)
+                time.sleep(10 * q_delay)
+                continue
+            if "meta" not in results:
+                print(results)
+                time.sleep(10 * q_delay)
+                continue
+            next_token = None
+            if "next_token" in results["meta"]:
+                next_token = results["meta"]["next_token"]
+            result_count = results["meta"]["result_count"]
+            time.sleep(q_delay)
+            page_idx += 1
+            num_results += result_count
+
+        with open(completed_path, "w") as f:
+            json.dump({"num_results": num_results, "num_pages": page_idx}, f)
+
+
+if __name__ == "__main__":
+    main()
