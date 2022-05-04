97d14265ae70d18c07ed97c7a19c38870d316b48
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-05-04 12:37:58 -0500

    Working on vaccine bert

--- pytorch_gleam/modeling/models/bert_pretrain.py
+++ pytorch_gleam/modeling/models/bert_pretrain.py
@@ -1,5 +1,6 @@
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import LambdaLR
+from transformers.optimization import Adafactor
 
 from pytorch_gleam.modeling.models.base_models import BaseLanguageModelForPreTraining
 
@@ -55,6 +56,16 @@ class BertPreTrainLanguageModel(BaseLanguageModelForPreTraining):
         params = self.parameters()
         if self.optimizer == "adamw":
             optimizer = AdamW(params, lr=self.learning_rate, weight_decay=0.0)
+        elif self.optimizer == "adafactor":
+            params = self._get_optimizer_params(self.weight_decay)
+            optimizer = Adafactor(
+                params,
+                lr=self.learning_rate,
+                weight_decay=self.weight_decay,
+                scale_parameter=False,
+                relative_step=False,
+                warmup_init=False,
+            )
         elif self.optimizer == "bert_decay":
             params = self._get_optimizer_params(self.weight_decay)
             optimizer = AdamW(
