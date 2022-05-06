58a59783a657416c01ef6da8e4daa9dc752b7270
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-05-06 10:00:56 -0500

    Working on vaccine bert

---
+++ pytorch_gleam/scripts/postprocess_bert.py
@@ -0,0 +1,48 @@
+import argparse
+import os
+
+import torch
+from transformers import BertConfig, BertModel, BertTokenizer
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--checkpoint_path", required=True)
+    parser.add_argument("-m", "--model_name", required=True)
+    parser.add_argument("-n", "--pre_model_name", required=True)
+    parser.add_argument("-o", "--output_path", required=True)
+    parser.add_argument("-p", "--prefix_count", default=2, type=int)
+    args = parser.parse_args()
+
+    checkpoint_path = args.checkpoint_path
+    model_name = args.model_name
+    pre_model_name = args.pre_model_name
+    output_path = args.output_path
+    prefix_count = args.prefix_count
+
+    config = BertConfig.from_pretrained(pre_model_name)
+    tokenizer = BertTokenizer.from_pretrained(pre_model_name)
+
+    model = BertModel(config)
+    checkpoint = torch.load(checkpoint_path, map_location="cpu")
+    weights = {".".join(k.split(".")[prefix_count:]): v for k, v in checkpoint["state_dict"].items()}
+    keys = model.load_state_dict(weights, strict=False)
+    if len(keys.missing_keys) > 0:
+        print("Missing keys:")
+        for key in keys.missing_keys:
+            print(f"  {key}")
+
+    if len(keys.unexpected_keys) > 0:
+        print("Unexpected keys:")
+        for key in keys.unexpected_keys:
+            print(f"  {key}")
+
+    model_path = os.path.join(output_path, model_name)
+
+    tokenizer.save_pretrained(model_path)
+    model.save_pretrained(model_path, save_config=True)
+    print(model_path)
+
+
+if __name__ == "__main__":
+    main()
