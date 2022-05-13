584b7623509c900343154a94ac17591b5eac4c83
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-05-13 11:27:02 -0500

    Contrastive frame experiments

--- pg_examples/cikm2022/clusters-v1.sh
+++ pg_examples/cikm2022/clusters-v1.sh
@@ -4,9 +4,14 @@
 #gleam predict --config pg_examples/cikm2022/ct-v11-pred-v1.yaml
 gleam predict \
 	--config pg_examples/cikm2022/ct-v11-pred.yaml \
+	--trainer.callbacks=TPURichProgressBar \
+	--trainer.callbacks=FitCheckpointCallback \
 	--trainer.callbacks=JsonlWriter \
 	--trainer.callbacks.output_path=/users/max/data/models/ct/ct-v11/predictions-same-Q1_Q19 \
-	--data.ContrastiveFrameDataModule \
+	--data=ContrastiveFrameDataModule \
+	--data.init_args.batch_size=8 \
+	--data.init_args.max_seq_len=128 \
+	--data.init_args.tokenizer_name=digitalepidemiologylab/covid-twitter-bert-v2 \
 	--data.init_args.label_name=candidates \
 	--data.init_args.predict_path=/shared/hltdir4/disk1/team/data/corpora/covid19-vaccine-twitter/v4/jsonl-non-rt/covid_candidates_1_19.json \
 	--data.init_args.predict_mode=same
@@ -25,7 +30,14 @@ python pytorch_gleam/scripts/contrastive_framing.py \
 	-o /users/max/data/models/ct/ct-v11/predictions/cluster-framings-Q1_Q19.jsonl
 
 # TODO take cmd input
-gleam predict --config pg_examples/cikm2022/ct-v11-pred-v4.yaml
+#gleam predict --config pg_examples/cikm2022/ct-v11-pred-v4.yaml
+gleam predict \
+	--config pg_examples/cikm2022/ct-v11-pred.yaml \
+	--trainer.callbacks=JsonlWriter \
+	--trainer.callbacks.output_path=/users/max/data/models/ct/ct-v11/predictions-all-Q1_Q19 \
+	--data.init_args.label_name=questions \
+	--data.init_args.predict_path=/users/max/data/models/ct/ct-v11/predictions/cluster-framings-Q1_Q19.jsonl \
+	--data.init_args.predict_mode=all
 
 python pytorch_gleam/scripts/contrastive_question_cluster.py \
 	-i /users/max/data/models/ct/ct-v11/predictions/cluster-framings-Q1_Q19.jsonl \
