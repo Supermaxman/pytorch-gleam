f1c6d7f883e70ae78fdf151b5d788d64a8ea7da4
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-05-13 11:16:38 -0500

    Contrastive frame experiments

--- pytorch_gleam/ex/gleam.py
+++ pytorch_gleam/ex/gleam.py
@@ -1,8 +1,13 @@
 import pytorch_lightning as pl
-from pytorch_lightning.utilities.cli import LightningCLI
+from pytorch_lightning.utilities.cli import CALLBACK_REGISTRY, DATAMODULE_REGISTRY, LightningCLI
+
+import pytorch_gleam.callbacks
+import pytorch_gleam.data.datasets
 
 
 def main():
+    CALLBACK_REGISTRY.register_classes(pytorch_gleam.callbacks, pl.Callback)
+    DATAMODULE_REGISTRY.register_classes(pytorch_gleam.data.datasets, pl.LightningDataModule)
     LightningCLI(
         pl.LightningModule,
         pl.LightningDataModule,
