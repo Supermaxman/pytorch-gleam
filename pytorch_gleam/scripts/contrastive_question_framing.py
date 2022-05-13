725278ea8567c23f463cc76e0db8ccb7b47ca44c
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-05-13 12:46:30 -0500

    Contrastive frame experiments

--- pytorch_gleam/scripts/contrastive_question_framing.py
+++ pytorch_gleam/scripts/contrastive_question_framing.py
@@ -3,16 +3,21 @@ import argparse
 from pytorch_gleam.data.twitter import preprocess_tweet, read_jsonl, TweetPreprocessConfig, write_jsonl
 
 
-def preprocess_example(example, preprocess_config):
-    ex_id = example["id"]
-    # sorted
-    all_docs = list(sorted(example["docs"], key=lambda x: x["size"], reverse=True))
-    doc = all_docs[0]
-    doc_txt = preprocess_tweet(doc["text"], preprocess_config)
+def parse_text(ex_text, preprocess_config):
+    doc_txt = preprocess_tweet(ex_text, preprocess_config)
     doc_txt = doc_txt.replace("\n ", " ").replace(" via ", " ")
     doc_txt = doc_txt.replace("twitteruser", " ").replace("twitterurl", " ")
     # TODO filter out hashtags?
     doc_txt = " ".join(doc_txt.split())
+    return doc_txt
+
+
+def preprocess_example(example, preprocess_config):
+    ex_id = example["id"]
+    # sorted
+    all_docs = list(sorted(example["docs"], key=lambda x: len(parse_text(x["text"], preprocess_config)), reverse=True))
+    doc = all_docs[0]
+    doc_txt = parse_text(doc["text"], preprocess_config)
     ex = {"id": ex_id, "text": doc_txt, "docs": all_docs, "size": sum([len(c["docs"]) for c in example["docs"]])}
     return ex
 
