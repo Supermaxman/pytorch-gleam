391dd2dfb70717593bf639141bc497d15b4951de
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-05-16 07:47:27 -0500

    Contrastive frame experiments

--- pytorch_gleam/scripts/sample_tweets.py
+++ pytorch_gleam/scripts/sample_tweets.py
@@ -28,15 +28,17 @@ def main():
 
     samples = np.random.randint(low=0, high=n, size=[q, t])
 
-    q_samples = {f'RQ{q_idx+1}': set([int(x) for x in samples[q_idx].tolist()]) for q_idx in range(q)}
+    q_samples = {f"RQ{q_idx+1}": set([int(x) for x in samples[q_idx].tolist()]) for q_idx in range(q)}
     outputs = []
     for t_idx, tweet in enumerate(tqdm(read_jsonl(input_path), total=n)):
         added = False
-        tweet['candidates'] = {}
+        if "data" in tweet:
+            tweet = tweet["data"]
+        tweet["candidates"] = {}
         for q_id, q_s in q_samples.items():
             if t_idx in q_s:
                 added = True
-                tweet['candidates'][q_id] = {}
+                tweet["candidates"][q_id] = {}
 
         if added:
             outputs.append(tweet)
