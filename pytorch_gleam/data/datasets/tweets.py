5c821af286833fb9ab2a721d573609945c7d4eda
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-09-27 09:33:12 -0500

    Added experiments

--- pytorch_gleam/data/datasets/tweets.py
+++ pytorch_gleam/data/datasets/tweets.py
@@ -169,3 +169,9 @@ def read_jsonl(path):
             if line:
                 ex = json.loads(line)
                 yield ex
+
+
+def write_jsonl(path, examples):
+    with open(path, "w") as f:
+        for ex in examples:
+            f.write(json.dumps(ex) + "\n")
