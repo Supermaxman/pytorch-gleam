536c5f1b89289c39745b66112e0c652efeb63849
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-10-13 15:29:33 -0500

    added ClearML support

--- pytorch_gleam/callbacks/clearml.py
+++ pytorch_gleam/callbacks/clearml.py
@@ -1,10 +1,22 @@
+import os
+from typing import Optional
+
+import pytorch_lightning as pl
+import yaml
 from clearml import Task
 from pytorch_lightning.callbacks import Callback
 
 
 class ClearMLTask(Callback):
-    def __init__(self, project_name: str, task_name: str):
+    def __init__(self, project_name: str):
         super().__init__()
         self.project_name = project_name
-        self.task_name = task_name
-        self.task = Task.init(project_name=self.project_name, task_name=self.task_name)
+
+    def setup(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", stage: Optional[str] = None) -> None:
+        task_name = os.path.basename(trainer.logger.save_dir)
+        self.task = Task.init(project_name=self.project_name, task_name=task_name)
+        config_path = os.path.join(trainer.logger.save_dir, "config.yaml")
+        self.task.connect_configuration(config_path)
+        with open(config_path) as f:
+            config = yaml.load(f)
+            self.task.connect(config)
