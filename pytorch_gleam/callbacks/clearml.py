1ae89ecd0c79604144316d621010e03d2cdbe08c
Max Weinzierl <maxwellweinzierl@gmail.com>
2022-10-13 15:49:46 -0500

    added ClearML support

--- pytorch_gleam/callbacks/clearml.py
+++ pytorch_gleam/callbacks/clearml.py
@@ -10,8 +10,11 @@ class ClearMLTask(Callback):
     def __init__(self, project_name: str):
         super().__init__()
         self.project_name = project_name
+        self.initialized = False
 
     def _init(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+        if self.initialized:
+            return
         task_name = os.path.basename(trainer.logger.save_dir)
         self.task = Task.init(project_name=self.project_name, task_name=task_name)
         config_path = os.path.join(trainer.logger.save_dir, "config.yaml")
@@ -19,19 +22,20 @@ class ClearMLTask(Callback):
         with open(config_path) as f:
             config = yaml.load(f)
             self.task.connect(config)
+        self.initialized = True
 
-    def on_train_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+    def on_train_epoch_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
         """Called when the train begins."""
         self._init(trainer, pl_module)
 
-    def on_validation_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+    def on_validation_epoch_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
         """Called when the validation loop begins."""
         self._init(trainer, pl_module)
 
-    def on_test_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+    def on_test_epoch_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
         """Called when the test begins."""
         self._init(trainer, pl_module)
 
-    def on_predict_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
+    def on_predict_epoch_start(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
         """Called when the predict begins."""
         self._init(trainer, pl_module)
