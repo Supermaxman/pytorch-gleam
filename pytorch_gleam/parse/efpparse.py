2c299fdf6c0ab76d9eee4d8e01fbae6566042b43
Max Weinzierl <supermaxman1@gmail.com>
2023-03-06 04:24:06 -0600

    Updated experiments

--- pytorch_gleam/parse/efpparse.py
+++ pytorch_gleam/parse/efpparse.py
@@ -260,7 +260,7 @@ def parse_tweets(tweet_path: str, num_processes: int, output_path: str):
             for ex_jsonl in tqdm(
                 p.imap_unordered(parse_tweet, read_jsonl(tweet_path)),
                 mininterval=1.0,
-                total=46_159_226,
+                total=13_221_468,
             ):
                 f.write(ex_jsonl)
 
