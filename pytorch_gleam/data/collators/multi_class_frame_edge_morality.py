b3e07e3fc90c22c733b99929e4ede0e4b16bfe8e
Max Weinzierl <supermaxman1@gmail.com>
2023-03-07 04:44:59 -0600

    Updated experiments

--- pytorch_gleam/data/collators/multi_class_frame_edge_morality.py
+++ pytorch_gleam/data/collators/multi_class_frame_edge_morality.py
@@ -9,8 +9,6 @@ class MultiClassFrameEdgeMoralityBatchCollator(BatchCollator):
         self.num_moralities = num_moralities
 
     def __call__(self, examples: list) -> dict:
-        if isinstance(examples[0], list) and len(examples) == 1:
-            examples = examples[0]
         pad_seq_len = self._calculate_seq_padding(examples)
 
         batch_size = len(examples)
