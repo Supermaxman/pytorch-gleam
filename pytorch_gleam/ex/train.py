c32e2645c6df9722e9c2b2e390db0dfca4359b4d
Max Weinzierl <supermaxman1@gmail.com>
2023-03-16 17:11:10 -0500

    Updated to torch 2.0 and lightning 2.0

--- pytorch_gleam/ex/train.py
+++ pytorch_gleam/ex/train.py
@@ -1,15 +1,18 @@
 import pytorch_lightning as pl
-from pytorch_lightning.utilities.cli import LightningCLI
+import torch
+from pytorch_lightning.cli import LightningCLI
 
 
 def main():
     cli = LightningCLI(
         pl.LightningModule,
         pl.LightningDataModule,
-        run=False,
         subclass_mode_model=True,
         subclass_mode_data=True,
+        save_config_kwargs={"overwrite": True},
+        run=False,
     )
+    cli.model = torch.compile(cli.model)
     cli.trainer.fit(cli.model, datamodule=cli.datamodule)
 
 
