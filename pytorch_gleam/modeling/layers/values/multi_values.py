ed5d7a730a7bae369b82b9731a0207f1cfd7d6fc
Max Weinzierl <maxwellweinzierl@gmail.com>
2023-11-15 15:08:31 -0600

    Cultural and moral values

--- pytorch_gleam/modeling/layers/values/multi_values.py
+++ pytorch_gleam/modeling/layers/values/multi_values.py
@@ -232,3 +232,88 @@ class MultiValuesExtraWeightedAttentionPooling(MultiValuesWeightedAttentionPooli
             "pooled_embedding": pooled_embedding,
             "output_features": pooled_embedding,
         }
+
+
+class MultiValuesQueryValueAttentionPooling(MultiValuesWeightedAttentionPooling):
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.moral_query = nn.Linear(self.input_dim, self.output_dim)
+        self.moral_value = nn.Linear(self.input_dim, self.output_dim)
+        self.cultural_value = nn.Linear(self.input_dim, self.output_dim)
+
+    def value_attention(self, embeddings, embeddings_mask, value_mask, value_embeddings):
+        # [bsize, seq_len, num_values]
+        # value_scores = torch.matmul(embeddings, self.value_embeddings.weight.t())
+        # [bsize, seq_len, 1, out_dim] * [1, 1, num_values, out_dim] -> [bsize, seq_len, num_values, out_dim]
+        value_scores = embeddings.unsqueeze(dim=2) * value_embeddings.weight.unsqueeze(dim=0).unsqueeze(dim=0)
+        # [bsize, seq_len, num_values]
+        value_scores = value_scores.sum(dim=-1)
+        # [bsize, seq_len, num_values] + [bsize, seq_len, 1]
+        value_scores = value_scores * embeddings_mask.unsqueeze(dim=-1).float()
+        # [bsize, num_values]
+        value_scores = value_scores.sum(dim=1)
+        # [bsize, num_values]
+        value_scores = value_scores + (1.0 - value_mask.float()) * -1e9
+        # [bsize, num_values]
+        value_probs = self.value_softmax(value_scores / (embeddings.shape[-1]) ** 0.5)
+        # [1, num_values, out_dim] * [bsize, num_values, 1]
+        value_embeddings = value_embeddings.weight.unsqueeze(dim=0) * value_probs.unsqueeze(dim=-1)
+        # [bsize, out_dim]
+        value_embedding = value_embeddings.sum(dim=1)
+        return value_embedding, value_probs
+
+    def attention_pooling(self, embeddings, embeddings_mask, value_embedding):
+        # [bsize, seq_len, out_dim] * [bsize, 1, out_dim] -> [bsize, seq_len, out_dim]
+        pooling_scores = embeddings * value_embedding.unsqueeze(dim=1)
+        # [bsize, seq_len, out_dim] -> [bsize, seq_len]
+        pooling_scores = pooling_scores.sum(dim=-1)
+        # [bsize, seq_len] + [bsize, seq_len]
+        pooling_scores = pooling_scores + (1.0 - embeddings_mask.float()) * -1e9
+        # [bsize, seq_len]
+        pooling_probs = self.pool_softmax(pooling_scores / (embeddings.shape[-1]) ** 0.5)
+        # [bsize, seq_len, out_dim] * [bsize, seq_len, 1] -> [bsize, seq_len, out_dim]
+        pooled_embeddings = embeddings * pooling_probs.unsqueeze(dim=-1)
+        # [bsize, out_dim]
+        pooled_embedding = pooled_embeddings.sum(dim=1)
+        return pooled_embedding, pooling_probs
+
+    def forward(self, embeddings, embeddings_mask, cultural_mask, moral_mask, post_mask, frame_mask):
+        cultural_queries = self.linear(embeddings)
+        moral_queries = self.moral_query(embeddings)
+        cultural_values = self.cultural_value(embeddings)
+        moral_values = self.moral_value(embeddings)
+
+        post_mask = self.combine_masks(embeddings_mask, post_mask)
+
+        frame_mask = self.combine_masks(embeddings_mask, frame_mask)
+
+        # value attention - which value info matters for frame
+        frame_cultural, frame_cultural_probs = self.value_attention(
+            cultural_queries, frame_mask, cultural_mask, self.cultural_embeddings
+        )
+        frame_moral, frame_moral_probs = self.value_attention(
+            moral_queries, frame_mask, moral_mask, self.moral_embeddings
+        )
+
+        # attention pooling - which post info matters for values
+        post_cultural, post_cultural_probs = self.attention_pooling(cultural_values, post_mask, frame_cultural)
+        post_moral, post_moral_probs = self.attention_pooling(moral_values, post_mask, frame_moral)
+
+        pooled_embedding = post_cultural + post_moral
+
+        return {
+            "frame_cultural": frame_cultural,
+            "frame_moral": frame_moral,
+            "frame_cultural_probs": frame_cultural_probs,
+            "frame_moral_probs": frame_moral_probs,
+            "post_cultural": post_cultural,
+            "post_moral": post_moral,
+            "post_cultural_probs": post_cultural_probs,
+            "post_moral_probs": post_moral_probs,
+            "pooled_embedding": pooled_embedding,
+            "output_features": pooled_embedding,
+        }
